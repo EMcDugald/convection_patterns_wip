7b4d77dc48d9be92d702ff3e87a5debcf4f91dbf
EMcdugald <edward.mcdugald@gmail.com>
2022-11-24 13:47:00 -0700

    stuff

---
+++ code/numerics/gifs/make_gif1.py
@@ -0,0 +1,29 @@
+import os
+import imageio
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import numpy as np
+#sys.path.append('/Users/edwardmcdugald/Research/convection_patterns_wip/code/numerics')
+#import convection_patterns as cp
+
+# cp.solveSH(20*np.pi,20*np.pi,256,256,.1,100,"SHAutoEncode_2",Rscale=.5,
+#            beta=.45,amplitude=.1,init_flag=1,energy=False)
+
+png_dir = "/Users/edwardmcdugald/Research/convection_patterns_wip/code/numerics/gifs/sh_gif_1"
+data = sio.loadmat("/Users/edwardmcdugald/Research/convection_patterns_wip/code/data/SHAutoEncode_2.mat")
+for idx in range(np.shape(data['uu'])[2]):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
+    ax.imshow(data['uu'][:,:,idx].T)
+    plt.savefig(png_dir+"/sh_"+f"{idx:0>4}"+".png", bbox_inches='tight')
+    plt.close()
+
+images = []
+for file_name in sorted(os.listdir(png_dir)):
+    if file_name.endswith('.png'):
+        file_path = os.path.join(png_dir, file_name)
+        images.append(imageio.imread(file_path))
+imageio.mimsave(png_dir+'/gif1.gif', images,fps=25)
+
+for f in os.listdir(png_dir):
+    if f.startswith('sh'):
+        os.remove(os.path.join(png_dir,f))
