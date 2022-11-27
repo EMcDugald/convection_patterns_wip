0d14b9a188432c49d9671036c73895f4a8760f2f
EMcdugald <edward.mcdugald@gmail.com>
2022-11-27 00:02:53 -0700

    more autoencoder stuff

--- code/ml_experiments/myPDEFIND.py
+++ code/ml_experiments/myPDEFIND.py
@@ -1,5 +1,27 @@
 import numpy as np
 from scipy.fft import fft2, ifft2, fftfreq
+import math
+
+def sin6hat(x):
+    res = np.sin(6.0*x)/3.0-3.0*np.sin(4.0*x)+15.0*np.sin(2.0*x)-20.0*x
+    res = -res/(20.0*math.pi)
+    return res
+
+def t6hat(rmax,rmin,x):
+   xx   = np.abs(x)
+   ones = np.ones_like(xx)
+   xmax = rmax*ones
+   xmin = rmin*ones
+   xx1 = np.minimum(xx,xmax)
+   xx2 = np.maximum(xx1,xmin)
+   xx3 = (ones - (xx2 - rmin)/(rmax-rmin))*math.pi
+   xx4 = sin6hat(xx3)
+   return xx4
+
+def gen_sigma_2(a,X1,X2,sc1,sc2):
+    q1center = np.array([0, 0])
+    z1 = t6hat(a/sc1, a/sc2 ,X1-q1center[0])*t6hat(a/sc1,a/sc2,X2-q1center[1])
+    return z1
 
 def BackwardDiff(u_curr, u_past, dt):
     """
